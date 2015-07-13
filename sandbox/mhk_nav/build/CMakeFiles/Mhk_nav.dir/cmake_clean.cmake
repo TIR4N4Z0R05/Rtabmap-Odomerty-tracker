ca786534609c8b59ff52f4a5ecaebf5c0dcd79a8
jiminotron <mhkazemi_engineer@yahoo.com>
2015-07-13 08:38:24 -0400

    this package about track of rtabmap version 0.0.0

---
+++ sandbox/mhk_nav/build/CMakeFiles/Mhk_nav.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Mhk_nav.dir/src/Ros_Snife.cpp.o"
+  "../bin/Mhk_nav.pdb"
+  "../bin/Mhk_nav"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Mhk_nav.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ sandbox/mhk_nav/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mhossein/SnifePackage_workspace/sandbox/mhk_nav/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/mhossein/SnifePackage_workspace/sandbox/mhk_nav/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/mhossein/SnifePackage_workspace/sandbox/mhk_nav/build/devel/lib:/home/mhossein/SnifePackage_workspace/sandbox/mhk_nav/build/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/mhossein/SnifePackage_workspace/sandbox/mhk_nav/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/mhossein/SnifePackage_workspace/sandbox/mhk_nav/build/devel/lib/pkgconfig:/home/mhossein/SnifePackage_workspace/sandbox/mhk_nav/build/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/mhossein/SnifePackage_workspace/sandbox/mhk_nav/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mhossein/SnifePackage_workspace/sandbox/mhk_nav/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/mhossein/SnifePackage_workspace/sandbox/mhk_nav:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
